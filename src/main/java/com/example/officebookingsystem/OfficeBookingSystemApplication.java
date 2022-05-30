70ea7fc6fe7d32f9b37b5f8f95c6a11760f10b99
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-05-30 18:51:58 +0700

    first commit

---
+++ src/main/java/com/example/officebookingsystem/OfficeBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.officebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/officebookingsystem/OfficeBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.officebookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
