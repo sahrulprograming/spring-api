751c95661413d5ab915f8e6413f9a090d1215e42
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-06-12 18:36:04 +0700

    Fixing some errors

--- src/main/java/com/example/officebookingsystem/controller/DistrictController.java
+++ src/main/java/com/example/officebookingsystem/controller/DistrictController.java
@@ -1,12 +1,4 @@
 package com.example.officebookingsystem.controller;
-
-<<<<<<< HEAD
-import com.example.officebookingsystem.service.DistrictService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-=======
 import com.example.officebookingsystem.domain.dto.request.DistrictCreateRequest;
 import com.example.officebookingsystem.service.DistrictService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +8,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/page")
->>>>>>> 3d11ab2fbbd9c757f0aa11ec800da6146a32a92d
 public class DistrictController {
 
     @Autowired
     private DistrictService districtService;
 
-<<<<<<< HEAD
-
-=======
     @PostMapping("/admin/district/create")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> createDistrict(@RequestBody DistrictCreateRequest districtCreateRequest){
@@ -36,5 +24,5 @@ public class DistrictController {
     public ResponseEntity<?> listDistrictByCityId(@PathVariable("id") Long id){
         return districtService.listDistrictByCity(id);
     }
->>>>>>> 3d11ab2fbbd9c757f0aa11ec800da6146a32a92d
+
 }
--- src/main/java/com/example/officebookingsystem/service/DistrictService.java
+++ src/main/java/com/example/officebookingsystem/service/DistrictService.java
@@ -1,37 +1,22 @@
 package com.example.officebookingsystem.service;
-<<<<<<< HEAD
 
 import com.example.officebookingsystem.domain.dto.response.DistrictResponse;
 import com.example.officebookingsystem.domain.entity.District;
-=======
 import com.example.officebookingsystem.domain.dto.request.DistrictCreateRequest;
 import com.example.officebookingsystem.domain.dto.response.DistrictCreateResponse;
 import com.example.officebookingsystem.domain.dto.response.MessageResponse;
 import com.example.officebookingsystem.domain.entity.City;
 import com.example.officebookingsystem.domain.entity.District;
 import com.example.officebookingsystem.domain.repository.CityRepository;
->>>>>>> 3d11ab2fbbd9c757f0aa11ec800da6146a32a92d
 import com.example.officebookingsystem.domain.repository.DistrictRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-<<<<<<< HEAD
-
-@Service
-public class DistrictService {
-
-//    @Autowired
-//    private DistrictRepository districtRepository;
-//
-//    public ResponseEntity<List<DistrictResponse>> listOfDistrict(){
-//
-//        return ResponseEntity.ok(districtRepository.findAll());
-//    }
-}
-=======
 import java.util.Optional;
+
+
 @Service
 public class DistrictService {
     @Autowired
@@ -69,4 +54,3 @@ public class DistrictService {
     }
 }
 
->>>>>>> 3d11ab2fbbd9c757f0aa11ec800da6146a32a92d
