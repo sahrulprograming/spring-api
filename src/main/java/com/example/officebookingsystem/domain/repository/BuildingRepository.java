a63496803c526e69310be1aa56126e02faf8f0ef
sofhiact <sofhiatambun@gmail.com>
2022-06-13 18:50:43 +0700

    updated user page

--- src/main/java/com/example/officebookingsystem/domain/repository/BuildingRepository.java
+++ src/main/java/com/example/officebookingsystem/domain/repository/BuildingRepository.java
@@ -1,10 +1,13 @@
 package com.example.officebookingsystem.domain.repository;
 
+import com.example.officebookingsystem.domain.dto.response.UserResponseBuilding;
 import com.example.officebookingsystem.domain.entity.Building;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,4 +16,13 @@ public interface BuildingRepository extends JpaRepository<Building,Long> {
     boolean existsByName(String name);
 
     List<Building> findAllByComplexId(Long complexId);
+
+    @Query(
+            value = "SELECT new com.example.officebookingsystem.domain.dto.response.UserResponseBuilding(b.id, b.name, s.name, b.address )"+
+                    "FROM Building b "+
+                    "INNER JOIN Complex c ON c.id=b.complex.id "+
+                    "INNER JOIN City s ON s.id=c.city.id"+
+                    "WHERE c.city.id=:id"
+    )
+    List<UserResponseBuilding> getAllBuildingsById(@Param("id") Long id);
 }
