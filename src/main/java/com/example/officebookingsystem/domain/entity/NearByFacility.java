6a707e2bcd676929105b2e18b9c6391174b22a60
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-06-15 13:43:24 +0700

    Adding Entity

---
+++ src/main/java/com/example/officebookingsystem/domain/entity/NearByFacility.java
@@ -0,0 +1,14 @@
+package com.example.officebookingsystem.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Facilities")
+public class NearByFacility {
+}
