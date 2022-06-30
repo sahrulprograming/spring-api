86f65b097a6e28aa8d16d93ae43cd1acaeab3742
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-06-30 21:19:36 +0700

    Finishing Upload and Update Profile

--- src/main/java/com/example/officebookingsystem/domain/dto/response/CityResponse.java
+++ src/main/java/com/example/officebookingsystem/domain/dto/response/CityResponse.java
@@ -3,13 +3,13 @@ package com.example.officebookingsystem.domain.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@AllArgsConstructor
+
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class CityResponse {
-    private String name;
+
     private Long id;
+    private String name;
     private Long province_id;
-
-
 }
--- src/main/java/com/example/officebookingsystem/domain/repository/CityRepository.java
+++ src/main/java/com/example/officebookingsystem/domain/repository/CityRepository.java
@@ -12,8 +12,8 @@ import java.util.List;
 public interface CityRepository extends JpaRepository <City, Long>{
         Boolean existsByName (String name);
 
-        @Query(value = "SELECT new com.example.officebookingsystem.domain.dto.response.CityResponse(c.name, c.id) FROM City c")
-        List<CityResponse> getAllCities();
+//        @Query(value = "SELECT new com.example.officebookingsystem.domain.dto.response.CityResponse(c.name, c.id) FROM City c")
+//        List<CityResponse> getAllCities();
 
         List<CityResponse> getCitiesByProvinceId(Long id);
 
