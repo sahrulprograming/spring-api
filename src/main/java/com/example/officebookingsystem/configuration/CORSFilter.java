3fb23b6ac33385ff20577d12306acfd2d7ce2301
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-07-04 11:59:39 +0700

    Fixing Cors Origin

--- src/main/java/com/example/officebookingsystem/configuration/CORSFilter.java
+++ src/main/java/com/example/officebookingsystem/configuration/CORSFilter.java
@@ -1,14 +1,28 @@
 package com.example.officebookingsystem.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter implements Filter {
+    @Value("${app.origin:*}")
+    private String origin;
+
+    @Value("${app.production:false}")
+    private boolean production;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -18,7 +32,17 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        if (production) {
+            List<String> origins = new ArrayList<>(Arrays.asList(origin.split(",")));
+            Optional<String> result = origins
+                    .stream()
+                    .filter(v -> v.equals(request.getHeader("Origin")))
+                    .findFirst();
+            result.ifPresent(s -> response.setHeader("Access-Control-Allow-Origin", s));
+        } else {
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        }
+//        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
